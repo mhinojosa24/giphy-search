209694bcbbc8cb27c4851db0e56c0182b9dcd3b4
Maximo Hinojosa <maximohinojosa13@gmail.com>
2018-11-11 19:58:38 -0800

    updated giphy-search

--- app.js
+++ app.js
@@ -7,35 +7,30 @@ var path = require('path')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use('/', express.static('public/'));  
+app.use(express.static('public'));
 
-app.get('/hello-gif', function (req, res) {
-  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif';
-  res.render('hello-gif', {gifUrl: gifUrl});
-});
+app.get('/', (req, res) => {
 
-app.get('/', function(req, res){
-    giphy.search(req.query.term, function(err, response){
-        res.render('home', {gifs: response.data})
+    var queryString = req.query.term;
+    var term = encodeURIComponent(queryString);
+    var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+
+    http.get(url, (response) => {
+        response.setEncoding('utf8');
+
+        var body = '';
+
+        response.on('data', (d) => {
+            // continuesly update data from giphy
+            body += d;
+        });
+
+        response.on('end', function() {
+            var parsed = JSON.parse(body);
+            res.render('home', {gifs: parsed.data});
+        });
     });
 });
-
 app.listen(3000, function() {
     console.log('Gif search on port localhost: 3000!');
 });
-
-
-
-
-// http.get(url, function(response){
-//     response.setEncoding('utf8');
-//     var body = '';
-//     response.on('data', function(d){
-//         // continuesly update data from giphy
-//         body += d;
-//     });
-//     response.on('end', function(){
-//         var parsed = JSON.parse(body);
-//         res.render('home', {gifs: parsed.data});
-//     });
-// });
